b963392150e972528cb89508e88a48619a47e3d9
Hiroki.T <hirodora@me.com>
2019-05-31 22:56:09 +0900

    add: save json

--- lib/utils.py
+++ lib/utils.py
@@ -4,11 +4,14 @@ from typing import List, Dict, ByteString
 
 
 class Face(object):
-    def __init__(self, json_info: Dict):
+    def __init__(self, json_info: Dict, image_filename: str):
         """
         Arg:
             json_info: result json from Face API
         """
+        self.image_filename = image_filename
+        self.json_data = json.dumps(json_info)
+        self.json_dict = json_info
         self.face_id = json_info['faceId']
         # face attributes
         for k, v in json_info['faceAttributes'].items():
@@ -61,7 +64,7 @@ class Client(object):
         }
         self.endpoint_url = url
 
-    def get(self, image_binary: ByteString) -> List[Face]:
+    def get(self, image_binary: ByteString, image_filename: str) -> List[Face]:
         """Send image to Face API server, and get result json
         Arg:
             image_binary: image (face included)
@@ -72,4 +75,4 @@ class Client(object):
             self.endpoint_url, params=self.params, headers=self.headers, data=image_binary)
         resp_json = response.json()
 
-        return [Face(resp_json[i]) for i in range(len(resp_json))]
+        return [Face(resp_json[i], image_filename) for i in range(len(resp_json))]
--- mface.py
+++ mface.py
@@ -3,8 +3,9 @@ import argparse
 import glob
 import os
 from concurrent.futures import ThreadPoolExecutor
-from lib.utils import Client
+import lib.utils
 import re
+from typing import List
 
 
 with open('api_config.json', 'r') as f:
@@ -12,19 +13,19 @@ with open('api_config.json', 'r') as f:
 
 FACE_API_URL = api_conf['face_api_url']
 SUBSCRIPTION_KEY = api_conf['subscription_key']
-c = Client(FACE_API_URL, SUBSCRIPTION_KEY)
+c = lib.utils.Client(FACE_API_URL, SUBSCRIPTION_KEY)
 
 
-def __target(image_filename):
+def __target(image_filename) -> List[lib.utils.Face]:
     with open(image_filename, 'rb') as img:
         image = img.read()
-        return c.get(image)
+        return c.get(image, image_filename)
 
 
 def main():
     parser = argparse.ArgumentParser(prog='mface', description='Microsoft Face API commandline tool')
     parser.add_argument('dir' ,help="images dir")
-    parser.add_argument('-f', '--format', choices=['json', 'csv'], default='json',help='Result data format')
+    parser.add_argument('-f', '--format', choices=['json'], default='json',help='Result data format')
     parser.add_argument('-s', '--save', default='mface_result', help='save filename')
     args = parser.parse_args()
 
@@ -32,7 +33,16 @@ def main():
     image_list = [os.path.abspath(x) for x in glob.glob(os.path.join(args.dir, '*')) if x.split('.')[-1] in ['png', 'jpg', 'jpeg']]
 
     result = list(executer.map(__target, image_list))
-    print(result)
+    
+    # save json
+    json_dict = []
+    for image in result:
+        filename = image[0].image_filename
+        json_dict.append([filename, [f.json_dict for f in image]])
+    
+    with open(args.save, 'w') as save:
+        save.write(json.dumps(json_dict, indent=4, separators=(',', ':', )))
+
 
 if __name__ == "__main__":
     main()
