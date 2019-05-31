82a8994820c7dd3be2647a88f9d5087379c5f059
Hiroki.T <hirodora@me.com>
2019-05-31 21:22:31 +0900

    add: Face & Client object

---
+++ lib/utils.py
@@ -0,0 +1,75 @@
+import requests
+import json
+from typing import List, Dict, ByteString
+
+
+class Face(object):
+    def __init__(self, json_info: Dict):
+        """
+        Arg:
+            json_info: result json from Face API
+        """
+        self.face_id = json_info['faceId']
+        # face attributes
+        for k, v in json_info['faceAttributes'].items():
+            self.__setattr__(k, v)
+        # face landmarks
+        for k, v in json_info['faceLandmarks'].items():
+            self.__setattr__(k, list(v.values()))
+
+        # face rectangle:
+        __face_rect = json_info['faceRectangle']
+        self.face_rectangle = [
+            __face_rect['left'], __face_rect['top'],
+            __face_rect['width'], __face_rect['height'],
+        ]  # [x, y, width, height]
+
+
+class Client(object):
+    """Microsoft Face API Client"""
+
+    def __init__(self, url: str, key: str):
+        """Initialize
+        Args:
+            url: endpoint url
+            key: access key
+        """
+        self.headers = {
+            'Ocp-Apim-Subscription-Key': key,
+            'Content-type': 'application/octet-stream',
+        }
+        self.get_face_attributes = [
+            'age',
+            'gender',
+            'headPose',
+            'smile',
+            'facialHair',
+            'glasses',
+            'emotion',
+            'hair',
+            'makeup',
+            'occlusion',
+            'accessories',
+            'blur',
+            'exposure',
+            'noise'
+        ]
+        self.params = {
+            'returnFaceId': 'true',
+            'returnFaceLandmarks': 'true',
+            'returnFaceAttributes': ','.join(self.get_face_attributes),
+        }
+        self.endpoint_url = url
+
+    def get(self, image_binary: ByteString) -> List[Face]:
+        """Send image to Face API server, and get result json
+        Arg:
+            image_binary: image (face included)
+        Return:
+            Faces list
+        """
+        response = requests.post(
+            self.endpoint_url, params=self.params, headers=self.headers, data=image_binary)
+        resp_json = response.json()
+
+        return [Face(resp_json[i]) for i in range(len(resp_json))]
--- main.py
+++ main.py
@@ -1,44 +1,15 @@
-import requests
 import json
+from lib.utils import Client
 
 with open('api_config.json', 'r') as f:
     api_conf = json.load(f)
 
-SUBSCRIPTION_KEY = api_conf['subscription_key']
 FACE_API_URL = api_conf['face_api_url']
+SUBSCRIPTION_KEY = api_conf['subscription_key']
 
-headers = {
-    'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
-    'Content-type': 'application/octet-stream',
-}
-
-get_face_attributes = [
-    'age',
-    'gender',
-    'headPose',
-    'smile',
-    'facialHair',
-    'glasses',
-    'emotion',
-    'hair',
-    'makeup',
-    'occlusion',
-    'accessories',
-    'blur',
-    'exposure',
-    'noise'
-]
-
-params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'true',
-    'returnFaceAttributes': ','.join(get_face_attributes),
-}
+c = Client(FACE_API_URL, SUBSCRIPTION_KEY)
 
 with open('test.jpg', 'rb') as img:
     image = img.read()
-
-response = requests.post(FACE_API_URL, params=params, headers=headers, data=image)
-
-data = json.dumps(response.json())
-print(data)
+    result = c.get(image)
+    print(result)
